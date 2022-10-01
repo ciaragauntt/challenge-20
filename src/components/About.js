99e3b5b0fe8345273783b0effe9c1ce4e4dba7e2
Ciara Gauntt <ci.gauntt@gmail.com>
2022-10-01 08:37:52 -0500

    added to about.js

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function About() {
+    return (
+    <div className="about-ciara">
+        {/* add image here */}
+        <article>
+            <h2>Get To Know Me!</h2>
+            <p>
+                Hello World! My name is Ciara Gauntt and I have just about completed my certification 
+                in full-stack web development through the University of Utah coding bootcamp. Some new 
+                updates and exciting news includes an internship with 'Marketing Design Team' where I 
+                will be learning about the business behind the development industry and add to my 
+                knowledge of UX/UI. I hope you enjoy looking at all the fun things I have built!
+                Please reach out if you have any questions or concerns!
+                ~ C 
+            </p>
+        </article>
+    </div>
+    );
+}
